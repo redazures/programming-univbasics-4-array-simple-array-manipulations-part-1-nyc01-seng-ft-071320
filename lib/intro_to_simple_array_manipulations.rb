904eeaa50963ecf09641adea6e0967ded1e5583e
james wu <jwu_nyc@yahoo.com>
2020-06-26 19:36:31 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,20 @@
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array,num)
+  2.times do
+    array.pop
+  end
+  end
+end
+
+def using_shift
+end
+
+def shift_with_args
+end
